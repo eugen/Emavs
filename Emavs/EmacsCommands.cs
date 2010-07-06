9b2654b5159914912bc7570a325efd9846320c10
c15c8ra1n <eugen.anghel@softhouse-int.com>
2010-07-06 16:27:56 +0300

    initial commit

---
+++ Emavs/EmacsCommands.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Emacs
+{
+    enum EmacsCommand
+    {
+        CancelCommand,
+
+        SetMark,
+        CharLeft,
+        CharRight,
+        WordLeft,
+        WordRight,
+        LineUp,
+        LineDown,
+
+        MoveBeginningOfLine,
+        MoveEndOfLine,
+        MoveBeginningOfFile,
+        MoveEndOfFile,
+
+        Yank,
+        KillRegion,
+        KillSave,
+        KillLine,
+    }
+}
