3739963aa3614f85adc575ea012f13a3c7a7c189
c15c8ra1n <eugen.anghel@softhouse-int.com>
2010-07-07 17:44:55 +0300

    better beginning-of-line handling; added recenter-top-bottom; added stubs for forward/backward-sexp

--- Emavs/EmacsCommandHandler.cs
+++ Emavs/EmacsCommandHandler.cs
@@ -15,6 +15,7 @@ namespace Emacs
         private string prevCommand;
         private bool emacsSelectionActive;
         private vsStartOfLineOptions prevStartOfLineOptions;
+        private int prevScrollLineOptions;
         private Dictionary<string, Action> handlers;
 
         private TextSelection Selection {
@@ -40,16 +41,17 @@ namespace Emacs
         }
 
         public void OnUnknownCommand() {
-            throw new NotImplementedException();
+            prevCommand = null;
         }
 
-        public void OnCommand(string CmdName) {
+        public void OnCommand(string cmdName) {
             Action action;
-            if (handlers.TryGetValue(CmdName, out action)) {
+            if (handlers.TryGetValue(cmdName, out action)) {
                 action();
             } else {
-                System.Windows.Forms.MessageBox.Show("Unknown command: " + CmdName);
+                System.Windows.Forms.MessageBox.Show("Unknown command: " + cmdName);
             }
+            prevCommand = cmdName;
         }
 
         private void OnCancelCommand() {
@@ -125,10 +127,36 @@ namespace Emacs
             } else {
                 prevStartOfLineOptions = vsStartOfLineOptions.vsStartOfLineOptionsFirstText;
             }
+            var oldPos = Selection.ActivePoint.AbsoluteCharOffset;
             Selection.StartOfLine(prevStartOfLineOptions, emacsSelectionActive);
+
+            // if we were already at the beginnig of the text, move to the beginning of the line
+            if (oldPos == Selection.ActivePoint.AbsoluteCharOffset) {
+                if (prevStartOfLineOptions == vsStartOfLineOptions.vsStartOfLineOptionsFirstText && !Selection.ActivePoint.AtStartOfLine) {
+                    prevStartOfLineOptions = vsStartOfLineOptions.vsStartOfLineOptionsFirstColumn;
+                    Selection.StartOfLine(vsStartOfLineOptions.vsStartOfLineOptionsFirstColumn, emacsSelectionActive);
+                } else if (prevStartOfLineOptions == vsStartOfLineOptions.vsStartOfLineOptionsFirstColumn) {
+                    prevStartOfLineOptions = vsStartOfLineOptions.vsStartOfLineOptionsFirstText;
+                    Selection.StartOfLine(vsStartOfLineOptions.vsStartOfLineOptionsFirstText);
+                }
+            }
         }
         private void OnMoveEndOfLine() {
             Selection.EndOfLine(emacsSelectionActive);
+        }
+        private void OnRecenterTopBottom() {
+            var value = 1;
+            if (prevCommand == "RecenterTopBottom")
+                value = (prevScrollLineOptions + 1) % 3;
+            var commandRing = new []{"Edit.ScrollLineBottom","Edit.ScrollLineCenter","Edit.ScrollLineTop"};
+            App.ExecuteCommand(commandRing[value]);
+            prevScrollLineOptions = value;
+        }
+        private void OnForwardSexp() {
+            throw new NotImplementedException();
+        }
+        private void OnBackwardSexp() {
+            throw new NotImplementedException();
         }
     }
 }
